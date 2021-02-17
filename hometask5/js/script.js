79abcdaf585eebd48dd800637f0c57894b67fe40
alex <adjyez@yahoo.com>
2021-02-17 21:14:37 +0300

    task 2.24 +

--- hometask5/js/script.js
+++ hometask5/js/script.js
@@ -1,21 +1,36 @@
-let ul = document.querySelector('.menu');
-let arrLI = document.querySelectorAll('li');
-// 1
+/* let menu = document.getElementsByClassName("menu")[0],
+    menuItem = document.getElementsByClassName("menu-item"),
+    title = document.getElementById("title"),
+    adv = document.getElementsByClassName("adv")[0],
+    promptforApple = document.querySelector("#prompt"),
+    menuItemLi = document.createElement("li"); */
+    let ul = document.querySelector('.menu'),
+    arrLI = document.querySelectorAll('li');
+
+//menu.insertBefore(menuItem[2], menuItem[1]);                    // Поменяли местами два элемента
 ul.insertBefore(arrLI[2],arrLI[1]);
-// 2
+
+/* menuItemLi.classList.add("menu-item");                          // Добавляем новый li, с классом и текстом
+menuItemLi.textContent = "Пятый элемент";                       
+menu.appendChild(menuItemLi);   */                                 
 let li = arrLI[1].cloneNode(true);
 li.textContent = 'Пятый элемент';
 ul.appendChild(li);
-// 3
+
+document.body.style.backgroundImage = "url('img/apple_true.jpg')";  // Меняем фон
+
+//title.textContent = "Мы продаем только подлинную технику Apple"    // Заменить текст
 document.getElementById('title').textContent = 'Мы продаем только подлинную технику Apple';
-// 4
+
+//adv.remove();                                                   // Удалить рекламу со страницы
 document.querySelector('.adv').remove();
-// 5
+
+/* let yourOpinion = prompt("Ваше отношение к технике Apple?");   // Отношение к технике Apple
+promptforApple.textContent = yourOpinion; */
 let input = prompt('Dfit отношение к технике apple', '');
-// 6
 document.getElementById('prompt').textContent = input;
 
-console.log(li);
+
 
 
 
